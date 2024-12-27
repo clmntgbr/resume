317f46a34e5e72e95855955ced8aef66a8f8771d
clmngbr <clement.goubier@gmail.com>
2024-12-27 09:54:36 +0100

    [main] Configure Astro site settings with base URL and trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://clmntgbr.github.io",
+  base: "resume",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
